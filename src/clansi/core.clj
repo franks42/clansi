ca469ebc96b4e44c10536f509bbf4789585610df
Frank Siebenlist <fsiebenlist@salesforce.com>
2013-12-12 19:21:08 -0800

    some cleanup and working cdoc macro...

--- src/clansi/core.clj
+++ src/clansi/core.clj
@@ -1,4 +1,12 @@
-(ns clansi.core)
+(ns clansi.core
+	"Collection of utilities to colorize terminal output by embedding ansi-code in the strings to print.
+ List of available ansi-code directives as keywords are: (keys ANSI-CODES).
+ A call like (ansi :blue), will return the ascii-code string to turn blue-printing on.
+ A number of helper functions are available to make the mark-up of strings with ansi-code directives easier:
+ (clansify \"this is printed in \" :red \"red\" :reset \", while this is \" :bright :green :underline \"bold&green&underlined.\")
+ In addition to the basic ansi-codes, one can also maintain combinations/sequence of codes in the *ANSI-STYLES* map. The map keys as keywords can be used as colorizing directives in the clansify function, like:
+ (clansify :protected \"this is protected text\" :reset \", while \" :unprotected \"this is an unprotected string.\")
+ Lastly, there is also a \"cdoc\" macro which is a colorized substitute for the venerable clojure.repl/doc macro.")
 
 (def ANSI-CODES
   {:reset              "[0m"
@@ -35,7 +43,13 @@
 (def ^:dynamic *ANSI-STYLES* 
 	{:white-on-black [:white :bg-black]
 	 :protected      [:green :bright] 
-	 :unprotected    [:red :bright]})
+	 :unprotected    [:red :bright]
+	 :line  :blue
+	 :title :bright
+	 :args  :red
+	 :macro :blue
+	 :doc   :green})
+
 
 (def ^:dynamic *use-ansi* "Rebind this to false if you don't want to see ANSI codes in some part of your code." true)
 
@@ -74,36 +88,8 @@
      ~@code))
 
 
-(defn style
-  "Applies ANSI color and style to a text string.
-
-   (style \"foo\" :red)
-   (style \"foo\" :red :underline)
-   (style \"foo\" :red :bg-blue :underline)
- "
-  [s & codes]
-  (str (apply str (map ansi codes)) s (ansi :reset)))
-
-(defn wrap-style
-  "Wraps a base string with a stylized wrapper.
-  If the wrapper is a string it will be placed on both sides of the base,
-  and if it is a seq the first and second items will wrap the base.
-
-  To wrap debug with red brackets => [debug]:
-
-  (wrap-style \"debug\" [\"[\" \"]\"] :red)
-  "
-  [base wrapper & styles]
-  (str (apply style (if (coll? wrapper) (first wrapper) wrapper) styles)
-       base
-       (apply style (if (coll? wrapper) (second wrapper) wrapper) styles)))
-
 (defn clansify-helper
-	"Takes a sequence of strings and keywords, where the keywords identify ansi-color-code directives.
-	The ansi-code directives only affect the strings that follow it. 
-	Returns a single string of the concatenations of the individual strings in the list, where each of these strings is prepended with the accumulated ansi-codes.
-	For example, (clansify \"this prints \" :red \"red\" :reset \" and that prints \" :blue \"blue\")
-	will print "
+	"Helper function for clansify that transforms a single sequence of keywords and strings without \"[..]\"."
 	[& s] 
 	(apply str  
 		(ansi :reset)
@@ -111,8 +97,9 @@
 			(for [c s] 
 				(if (keyword? c) 
 					(if-let [codes (c *ANSI-STYLES*)]
-						(apply str (map ansi codes)) 
-						(ansi c))
+						(let [codes (if (sequential? codes) codes [codes])]
+							(apply str (map ansi codes)))
+							(ansi c))
 					(if-let [codes (:ansi-codes (meta c))]
 						(str (apply str (map ansi codes)) c)
 						c))))
@@ -120,7 +107,14 @@
 
 
 (defn clansify
-	""
+	"Takes a sequence of strings and keywords, where the keywords identify ansi-color-code directives.
+	The ansi-code directives only affect the strings that follow it. 
+	Returns a single string of the concatenations of the individual strings in the list, where each of these strings is prepended with the accumulated ansi-codes.
+	Example with ansi-codes:
+	(clansify \"this is printed in \" :red \"red\" :reset \", while this is \" :bright :green :underline \"bold&green&underlined.\") 
+	Example with style-codes defined in *ANSI-STYLES* map:
+	(clansify :protected \"this is protected text\" :reset \", while \" :unprotected \"this is an unprotected string.\")
+"
 	[& ansified-strings] 
 	(apply str (first 	
 		(reduce 
@@ -146,57 +140,84 @@
   "Print the list of supported ANSI styles, each style name shown
   with its own style."
   []
+  (println "ansi-codes:")
   (doall
-    (map #(println (style (name %) %)) (sort-by name (keys ANSI-CODES))))
+    (map #(println (clansify % (name %))) (sort-by name (keys ANSI-CODES))))
+  (println "*ANSI-STYLES* codes:")
+  (doall
+    (map #(println (clansify % (name %))) (sort-by name (keys *ANSI-STYLES*))))
   nil)
-
-(def doc-style* (ref {:line  :blue
-                      :title :bright
-                      :args  :red
-                      :macro :blue
-                      :doc   :green}))
 
 (defn print-special-doc-color
   "Print stylized special form documentation."
-  [name type anchor]
-  (println (style "-------------------------" (:line @doc-style*)))
-  (println (style name (:title @doc-style*)))
-  (println type)
-  (println (style (str "  Please see http://clojure.org/special_forms#" anchor)
-                  (:doc @doc-style*))))
+  [meta-map]
+  (println (clansify :line "-------------------------"))
+  (println (clansify :title (:name meta-map)))
+  (when-let [f (:forms meta-map)]
+  	(doseq [s f] 
+  	  (println (str "  " (clansify :args s)))))
+  (println (clansify :macro "Special Form"))
+  (println (str "  " (clansify :doc (:doc meta-map))))
+  (println (clansify :doc (str "  Please see http://clojure.org/special_forms#" (:name meta-map)))))
 
 (defn print-namespace-doc-color
   "Print stylized documentation for a namespace."
   [nspace]
-  (println (style "-------------------------"    (:line @doc-style*)))
-  (println (style (str (ns-name nspace))         (:title @doc-style*)))
-  (println (style (str " " (:doc (meta nspace))) (:doc @doc-style*))))
+  (println (clansify :line "-------------------------"))
+  (println (clansify :title (str (ns-name nspace))))
+  (println (clansify :macro "Namespace"))
+  (println (clansify :doc (str " " (:doc (meta nspace))))))
 
 (defn print-doc-color
   "Print stylized function documentation."
   [v]
-  (println (style "-------------------------" (:line @doc-style*)))
-  (println (style (str (ns-name (:ns (meta v))) "/" (:name (meta v)))
-                  (:title @doc-style*)))
+  (println (clansify :line "-------------------------"))
+  (println (clansify :title (str (ns-name (:ns (meta v))) "/" (:name (meta v)))))
   (print "(")
   (doseq [alist (:arglists (meta v))]
-   (print "[" (style (apply str (interpose " " alist)) (:args @doc-style*)) "]"))
+   (print "[" (clansify :args (apply str (interpose " " alist))) "]"))
   (println ")")
 
   (when (:macro (meta v))
-    (println (style "Macro" (:macro @doc-style*))))
-  (println "  " (style (:doc (meta v)) (:doc @doc-style*))))
+    (println (clansify :macro "Macro")))
+  (println "  " (clansify :doc (:doc (meta v)))))
 
-(defmacro color-doc
-  "A stylized version of clojure.core/doc."
-  [v]
-  `(binding [print-doc print-doc-color
-             print-special-doc print-special-doc-color
-             print-namespace-doc print-namespace-doc-color]
-     (doc ~v)))
 
-(defn colorize-docs
-  []
-  (intern 'clojure.core 'print-doc print-doc-color)
-  (intern 'clojure.core 'print-special-doc print-special-doc-color)
-  (intern 'clojure.core 'print-namespace-doc print-namespace-doc-color))
+(defmacro cdoc
+  "Prints colorized documentation for a var, special form or namespace given its name"
+  [name]
+  (if-let [special-name ('#{& fn catch try finally} name)]
+    (#'print-special-doc-color (#'clojure.repl/special-doc special-name))
+    (cond
+      (#'clojure.repl/special-doc-map name) 
+      	`(#'print-special-doc-color (#'clojure.repl/special-doc '~name))
+      (find-ns name) `(#'print-namespace-doc-color (find-ns '~name))
+      (resolve name) `(#'print-doc-color (var ~name)))))
+
+
+;;;;;;; not sure if we need the previous style and wrapper function anymore...
+
+(defn style
+  "Applies ANSI color and style to a text string.
+
+   (style \"foo\" :red)
+   (style \"foo\" :red :underline)
+   (style \"foo\" :red :bg-blue :underline)
+ "
+  [s & codes]
+  (str (apply str (map ansi codes)) s (ansi :reset)))
+
+(defn wrap-style
+  "Wraps a base string with a stylized wrapper.
+  If the wrapper is a string it will be placed on both sides of the base,
+  and if it is a seq the first and second items will wrap the base.
+
+  To wrap debug with red brackets => [debug]:
+
+  (wrap-style \"debug\" [\"[\" \"]\"] :red)
+  "
+  [base wrapper & styles]
+  (str (apply style (if (coll? wrapper) (first wrapper) wrapper) styles)
+       base
+       (apply style (if (coll? wrapper) (second wrapper) wrapper) styles)))
+
